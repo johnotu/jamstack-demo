2470c1341795095fb3bc392d3e9717c014800e76
John Otu <johnne01@gmail.com>
2018-08-28 21:03:59 +0000

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>This is me trying out Gatsby, for the JAMstack conf.</p>
+    <p>Let's see what Gatsby can do!</p>
+    {/* <Link to="/page-2/">Go to page 2</Link> */}
+  </div>
+)
+
+export default IndexPage
